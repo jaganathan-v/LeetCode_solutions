1134f5a8630a87ce603ed6e44823058ce2d28308
jaganathan-v <jaganathan7v@gmail.com>
2026-08-09 22:51:18 +0530

    Time: 2 ms (99.63%) | Memory: 44.6 MB (56.98%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,14 +1,28 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-    HashMap<Character,Integer> hm = new HashMap<> ();
-    HashMap<Character,Integer> hm1 = new HashMap<> ();
-    for(char c : s.toCharArray()){
-        hm.put(c,hm.getOrDefault(c,0)+1);
-    } 
-    for(char c : t.toCharArray()){
-        hm1.put(c,hm1.getOrDefault(c,0)+1);
+    // HashMap<Character,Integer> hm = new HashMap<> ();
+    // HashMap<Character,Integer> hm1 = new HashMap<> ();
+    // for(char c : s.toCharArray()){
+    //     hm.put(c,hm.getOrDefault(c,0)+1);
+    // } 
+    // for(char c : t.toCharArray()){
+    //     hm1.put(c,hm1.getOrDefault(c,0)+1);
+    // }
+    // boolean isIdentical = hm.equals(hm1); 
+    // return isIdentical;
+    if(s.length()!= t.length()) return false;
+    int[] arr = new int[26];
+    for(int c:s.toCharArray()){
+        arr[c - 'a']++;
     }
-    boolean isIdentical = hm.equals(hm1); 
-    return isIdentical;
+     for(int c:t.toCharArray()){
+        arr[c - 'a']--;
+    }
+    for(int n:arr){
+        if(n != 0){
+            return false;
+        }
+    }
+    return true;
     }
 }
