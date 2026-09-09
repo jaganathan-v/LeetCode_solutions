53ff04fd934c7fe40d269eac6e2c64584ecd86c7
jaganathan-v <jaganathan7v@gmail.com>
2026-09-09 10:39:28 +0530

    Time: 0 ms (100.00%) | Memory: 46.9 MB (16.37%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -11,18 +11,31 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-    if(head == null){
+        if(head == null){
         return false;
     }
-    HashSet<ListNode> hs = new HashSet<>();
-    ListNode temp = head;
-    while(temp.next != null){
-        if(hs.contains(temp)){
-            return true;
-        }
-        hs.add(temp);
-        temp = temp.next;
-    }
-    return false;  
+    ListNode slowPointer = head;
+    ListNode fastPointer = head;
+    while(fastPointer != null && fastPointer.next!= null){
+		 slowPointer = slowPointer.next;
+		 fastPointer = fastPointer.next.next;
+             if(slowPointer == fastPointer) {
+			 return true;
+		 }
+	  }
+	  return false;
+    // if(head == null){
+    //     return false;
+    // }
+    // HashSet<ListNode> hs = new HashSet<>();
+    // ListNode temp = head;
+    // while(temp.next != null){
+    //     if(hs.contains(temp)){
+    //         return true;
+    //     }
+    //     hs.add(temp);
+    //     temp = temp.next;
+    // }
+    // return false;  
     }
 }
