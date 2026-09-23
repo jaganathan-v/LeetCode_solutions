262baa9840da910779271b9428c6f5b72846f180
jaganathan-v <jaganathan7v@gmail.com>
2026-09-23 20:13:07 +0530

    Time: 0 ms (100.00%) | Memory: 46.9 MB (16.65%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -11,31 +11,42 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        if(head == null){
-        return false;
-    }
-    ListNode slowPointer = head;
-    ListNode fastPointer = head;
-    while(fastPointer != null && fastPointer.next!= null){
-		 slowPointer = slowPointer.next;
-		 fastPointer = fastPointer.next.next;
-             if(slowPointer == fastPointer) {
-			 return true;
-		 }
-	  }
-	  return false;
-    // if(head == null){
+    //     if(head == null){
     //     return false;
     // }
-    // HashSet<ListNode> hs = new HashSet<>();
-    // ListNode temp = head;
-    // while(temp.next != null){
-    //     if(hs.contains(temp)){
-    //         return true;
-    //     }
-    //     hs.add(temp);
-    //     temp = temp.next;
-    // }
-    // return false;  
+    // ListNode slowPointer = head;
+    // ListNode fastPointer = head;
+    // while(fastPointer != null && fastPointer.next!= null){
+	// 	 slowPointer = slowPointer.next;
+	// 	 fastPointer = fastPointer.next.next;
+    //          if(slowPointer == fastPointer) {
+	// 		 return true;
+	// 	 }
+	//   }
+	//   return false;
+    // // if(head == null){
+    // //     return false;
+    // // }
+    // // HashSet<ListNode> hs = new HashSet<>();
+    // // ListNode temp = head;
+    // // while(temp.next != null){
+    // //     if(hs.contains(temp)){
+    // //         return true;
+    // //     }
+    // //     hs.add(temp);
+    // //     temp = temp.next;
+    // // }
+    // // return false;  
+    ListNode slow = head;
+    ListNode fast = head;
+    while(fast != null && fast.next != null){
+        slow = slow.next;
+        fast = fast.next.next;
+        if(fast == slow){
+            return true;
+        }
+    }
+    return false;
+
     }
 }
